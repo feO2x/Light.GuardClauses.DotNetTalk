1c38afb4941668ceb82d5c9b665f7ea6c2a19fa8
Kenny Pflug <kenny.pflug@live.de>
2016-05-30 09:40:18 +0200

    added MustNotBeNull example

---
+++ Light.GuardClauses.Examples/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.GuardClauses.Examples
+{
+    public class Account
+    {
+        public Account(string name)
+        {
+            name.MustNotBeNull(nameof(name));
+
+            // MustNotBeNull replaces the probably most-common precondition check
+            //if (name == null)
+            //    throw new ArgumentNullException(nameof(name));
+
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    public class SomeTests
+    {
+        [Fact]
+        public void ConstructorThrowsExceptionOnNull()
+        {
+            Action act = () => new Account(null);
+
+            act.ShouldThrow<ArgumentNullException>()
+               .And.ParamName.Should().Be("name");
+        }
+    }
+}
